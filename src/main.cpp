d5f66724f99ed0974b703a5e88db4655128c9deb
marck2325 <A23310375@live.ceti.mx>
2024-10-18 23:32:48 +0000

    clase 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include<porcentaje.hpp>
+#include<list>
 int main(){
+    std::list<porcentaje>focos;
     porcentaje foco1;
     porcentaje foco2;
     porcentaje foco3;
+
+    focos.emplace_back(foco1);
+    focos.emplace_back(foco2);
+    focos.emplace_back(foco3);
+
+    for (auto &&f : focos)
+    {
+        f.Incrementar(10);
+    }
+
 
     foco1.Incrementar(10);
     foco1.Incrementar(10);
     foco1.Disminuir(4);
     std::cout<<"Foco 1 :"<<foco1.Imprimir()<<std::endl;
+
 }
